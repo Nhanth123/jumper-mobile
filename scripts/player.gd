9735651b24ec55cf34ce4955a40696e7143bca1b
nhan.trinh <hongnhan50@gmail.com>
2024-05-04 13:23:23 +0700

    Feature: Implement player's movement on edges.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 
 var speed = 300
+var viewport_size
+
+func _ready():
+	viewport_size = get_viewport_rect().size
 
 func _physics_process(delta):
 	var direction = Input.get_axis("move_left","move_right")
@@ -8,5 +12,11 @@ func _physics_process(delta):
 		velocity.x = speed * direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-	
+		
 	move_and_slide()
+	
+	var margin = 20
+	if global_position.x > viewport_size.x + margin:
+		global_position.x = -margin
+	if global_position.x < -margin:
+		global_position.x = viewport_size.x + margin
