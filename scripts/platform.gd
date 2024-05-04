508120210caea9327afb0cd7cd4f3c99105e6c0d
nhan.trinh <hongnhan50@gmail.com>
2024-05-04 16:53:13 +0700

    Feature: Implement player's jump and animation

--- scripts/platform.gd
+++ scripts/platform.gd
@@ -3,4 +3,5 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body is Player:
-		print("body enter the platform")
+		if body.velocity.y > 0:
+			body.jump()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,25 @@
 extends CharacterBody2D
 class_name Player
 
+@onready var animator = $AnimationPlayer
+
 var speed = 300.0
 var viewport_size
 var gravity = 15.0
 var max_fall_velocity = 1000.0
+var jump_velocity = -800
+var margin = 20
 
 func _ready():
 	viewport_size = get_viewport_rect().size
+
+func _process(delta):
+	if velocity.y > 0:
+		if animator.current_animation != "fall":
+			animator.play("fall")
+	elif velocity.y < 0:
+		if animator.current_animation != "jump":
+			animator.play("jump")
 
 func _physics_process(delta):
 	velocity.y += gravity
@@ -22,8 +34,10 @@ func _physics_process(delta):
 		
 	move_and_slide()
 	
-	var margin = 20
 	if global_position.x > viewport_size.x + margin:
 		global_position.x = -margin
 	if global_position.x < -margin:
 		global_position.x = viewport_size.x + margin
+
+func jump():
+	velocity.y = jump_velocity
