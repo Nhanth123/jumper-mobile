8307c67fc00e9beedb27c20f8c053110649a780c
nhan.trinh <hongnhan50@gmail.com>
2024-05-04 14:22:18 +0700

    Feature: Implement gravity for player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,18 @@
 extends CharacterBody2D
 
-var speed = 300
+var speed = 300.0
 var viewport_size
+var gravity = 15.0
+var max_fall_velocity = 1000.0
 
 func _ready():
 	viewport_size = get_viewport_rect().size
 
 func _physics_process(delta):
+	velocity.y += gravity
+	if velocity.y > max_fall_velocity:
+		velocity.y = max_fall_velocity
+	
 	var direction = Input.get_axis("move_left","move_right")
 	if direction:
 		velocity.x = speed * direction
