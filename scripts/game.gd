bfaef581fe6d058ec6948a00d7e4eab08e613fe3
nhan.trinh <hongnhan50@gmail.com>
2024-05-06 11:06:17 +0700

    Feature: Implement platform for player in game.

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,13 +1,26 @@
 extends Node2D
 
-var camera_scene = preload("res://scenes/game_camera.tscn")
+@onready var platform_parent = $PlatformParent
 
+var camera_scene = preload("res://scenes/game_camera.tscn")
+var platform_scene = preload("res://scenes/platform.tscn")
 var camera = null
 
 func _ready():
 	camera = camera_scene.instantiate()
 	camera.setup_camera($Player)
 	add_child(camera)
+	
+	create_platform(Vector2(100, 300))
 
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+
+func create_platform(location: Vector2):
+	var platform = platform_scene.instantiate()
+	platform.global_position = location
+	platform_parent.add_child(platform)
+	return platform
