f24ef6a083635256f90c015be9464f913ca5a1cb
nhan.trinh <hongnhan50@gmail.com>
2024-05-06 16:16:38 +0700

    Feature: Creating the ground for player.

--- scripts/game.gd
+++ scripts/game.gd
@@ -5,13 +5,20 @@ extends Node2D
 var camera_scene = preload("res://scenes/game_camera.tscn")
 var platform_scene = preload("res://scenes/platform.tscn")
 var camera = null
-
+var platform_width
 func _ready():
 	camera = camera_scene.instantiate()
 	camera.setup_camera($Player)
 	add_child(camera)
 	
-	create_platform(Vector2(100, 300))
+	# Creating the ground
+	var viewport_size = get_viewport_rect().size
+	platform_width = 136
+	var ground_layer_platform_count = (viewport_size.x / platform_width) + 1
+	var ground_layer_y_offset = 62
+	for i in range(ground_layer_platform_count):
+		var ground_location = Vector2(i * platform_width , viewport_size.y - ground_layer_y_offset)
+		create_platform(ground_location)
 
 func _process(delta):
 	if Input.is_action_just_pressed("reset"):
