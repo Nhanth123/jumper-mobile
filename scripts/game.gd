f4314255ac64a12e901c389c2b6322d36efb7f6f
nhan.trinh <hongnhan50@gmail.com>
2024-05-08 12:45:01 +0700

    Feature: creating ground and level for player.

--- scripts/game.gd
+++ scripts/game.gd
@@ -10,36 +10,19 @@ var ground_layer_y_offset = 62
 var start_position_y
 var y_distance_between_platforms = 100
 var level_size = 50
+var viewport_size
+var generated_plaform_count = 0
 
 func _ready():
 	camera = camera_scene.instantiate()
 	camera.setup_camera($Player)
 	add_child(camera)
 	
-	# Creating the ground
-	var viewport_size = get_viewport_rect().size
+	viewport_size = get_viewport_rect().size
+	generated_plaform_count = 0
+	start_position_y = viewport_size.y - (y_distance_between_platforms * 2)
+	generate_level(start_position_y, true)
 	
-	platform_width = 136
-	var ground_layer_platform_count = (viewport_size.x / platform_width) + 1
-	
-	for i in range(ground_layer_platform_count):
-		var ground_location = Vector2(i * platform_width , viewport_size.y - ground_layer_y_offset)
-		create_platform(ground_location)
-	
-	#Generate the rest of level
-	print(viewport_size.y)
-	var start_position_y = viewport_size.y - (y_distance_between_platforms * 2)
-	
-	for i in range(level_size):
-		var max_x_position = viewport_size.x - platform_width
-		var random_x = randf_range(0.0, max_x_position)
-		
-		var location : Vector2
-		location.x = random_x
-		location.y = start_position_y - (y_distance_between_platforms * i)
-		print(location)
-		create_platform(location)
-
 func _process(delta):
 	if Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
@@ -51,3 +34,27 @@ func create_platform(location: Vector2):
 	platform.global_position = location
 	platform_parent.add_child(platform)
 	return platform
+
+func generate_level(start_y: float, generate_ground: bool):
+	platform_width = 136
+	
+	# Creating the ground
+	if generate_ground == true:
+		var ground_layer_platform_count = (viewport_size.x / platform_width) + 1
+		
+		for i in range(ground_layer_platform_count):
+			var ground_location = Vector2(i * platform_width , viewport_size.y - ground_layer_y_offset)
+			create_platform(ground_location)
+	
+	#Generate the rest of level
+	for i in range(level_size):
+		var max_x_position = viewport_size.x - platform_width
+		var random_x = randf_range(0.0, max_x_position)
+		
+		var location : Vector2
+		location.x = random_x
+		location.y = start_y - ( y_distance_between_platforms * i )
+		create_platform(location)
+		generated_plaform_count += 1
+	print(generated_plaform_count)
+	
