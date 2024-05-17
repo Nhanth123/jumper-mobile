76b55b64e80c12756529afc3368f740fd2721f4b
nhan.trinh <hongnhan50@gmail.com>
2024-05-17 12:07:24 +0700

    Feature: Implement screen debug

--- scripts/screens.gd
+++ scripts/screens.gd
@@ -1,1 +1,12 @@
 extends CanvasLayer
+
+@onready var console  = $Debug/ConsoleLog
+
+func _ready():
+	console.visible = false
+	
+func _process(delta):
+	pass
+
+func _on_toggle_console_pressed():
+	console.visible = !console.visible
