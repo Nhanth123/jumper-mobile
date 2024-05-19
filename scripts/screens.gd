65220c321ef976def1c292c48667110760740639
nhan.trinh <hongnhan50@gmail.com>
2024-05-19 23:55:37 +0700

    Implement screen to capture button.

--- scripts/screens.gd
+++ scripts/screens.gd
@@ -5,8 +5,35 @@ extends CanvasLayer
 func _ready():
 	console.visible = false
 	
+	register_buttons()
+	
 func _process(_delta):
 	pass
+
+func register_buttons():
+	var group_buttons = get_tree().get_nodes_in_group("buttons")
+	if group_buttons.size() > 0:
+		for button in group_buttons:
+			if button is ScreenButton:
+				button.clicked.connect(_on_button_pressed)
+
+func _on_button_pressed(button):
+	match button.name:
+		"TitlePlay":
+			print("Title play pressed")
+		"PauseText":
+			print("PausePlay pressed")
+		"PauseRetry":
+			print("PauseRetry")
+		"PauseBack":
+			print("PauseBack")
+		"PauseClose":
+			print("PauseClose")
+		"GameOverRetry":
+			print("GameOverRetry")
+		"GameOverBack":
+			print("GameOverBack")
+			
 
 func _on_toggle_console_pressed():
 	console.visible = !console.visible
