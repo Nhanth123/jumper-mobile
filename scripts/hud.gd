38a55152e707699f2d146d297c49e62e8010bf3e
nhan.trinh <hongnhan50@gmail.com>
2024-05-26 23:37:01 +0700

    Feature: Safe area for phone-screen.

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,5 +1,20 @@
 extends Control
 
+@onready var topbar = $TopBar
+@onready var topbar_bg = $TopBarBG
+
+func _ready():
+	var os_name = OS.get_name()
+	if os_name == "Android" || os_name == "iOS":
+		var safe_area = DisplayServer.get_display_safe_area()
+		var safe_area_top = safe_area.position.y
+		
+		topbar.position.y += safe_area_top
+		
+		var margin = 10
+		topbar_bg.size.y += safe_area_top + margin
+		
+		 
 
 func _on_pause_button_pressed():
 	pass # Replace with function body.
