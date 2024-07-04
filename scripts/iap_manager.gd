cd56f8879dcba86b0ebdc4c25c890c0e56a1b540
nhan.trinh <hongnhan50@gmail.com>
2024-07-04 16:55:48 +0700

    Feature: Implement google plugin

--- scripts/iap_manager.gd
+++ scripts/iap_manager.gd
@@ -2,5 +2,16 @@ extends Node
 
 signal unlock_new_skin
 
+var google_payment = null
+
+
+func _ready():
+	if Engine.has_singleton("GodotGooglePlayBilling"):
+		google_payment = Engine.get_singleton("GodotGooglePlayBilling")
+		MyUtility.add_log_msg("Android IAP support is enabled")
+	else:
+		MyUtility.add_log_msg("Android IAP support is not available")	
+	
+
 func purchase_skin():
 	unlock_new_skin.emit()
