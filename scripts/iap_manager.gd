b23b2e83ef601a331f3bfef54f59666f142bef18
nhan.trinh <hongnhan50@gmail.com>
2024-07-07 23:28:57 +0700

    Feature: query product info on google console

--- scripts/iap_manager.gd
+++ scripts/iap_manager.gd
@@ -3,6 +3,7 @@ extends Node
 signal unlock_new_skin
 
 var google_payment = null
+var new_skin_sku = "new_player_skin" # need to be matched on google console
 
 
 func _ready():
@@ -13,6 +14,9 @@ func _ready():
 		google_payment.connected.connect(_on_connected)
 		google_payment.connect_error.connect(_on_connect_error)
 		google_payment.disconnected.connect(_on_disconnected)
+		google_payment.sku_details_query_completed.connect(_on_sku_details_query_completed)
+		
+		google_payment.sku_details_query_error.connect(_on_sku_details_query_error)
 		
 		google_payment.startConnection()
 	else:
@@ -24,9 +28,21 @@ func purchase_skin():
 
 func _on_connected():
 	MyUtility.add_log_msg("Connected")
+	google_payment.querySkuDetails([new_skin_sku], "inapp")
 
 func _on_connect_error(response_id, debug_msg):
 	MyUtility.add_log_msg("Error occured, response id: " + str(response_id) + ". Debug msg: " + debug_msg)
 
 func _on_disconnected():
 	MyUtility.add_log_msg("Disconnected from googlepayment")
+
+func _on_sku_details_query_completed(skus):
+	MyUtility.add_log_msg("sku detail query comoleted")
+	
+	for sku in skus:
+		MyUtility.add_log_msg("SKUs")
+		MyUtility.add_to_group(str(sku))
+
+
+func _on_sku_details_query_error(response_id, error_message, skus):
+	MyUtility.add_log_msg("Sku query error , response id: " + str(response_id) + " ,message: " + str(error_message) + ", skus: " + str(skus) )
